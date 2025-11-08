d926e98755b71d9fb43dd3805428caf8aee8d1ac
ltb <ltb@DESKTOP-FO8H3TR>
2025-11-09 03:33:35 +0800

    feat(首页特性): 添加FDE特性链接及悬停样式
    - 在首页特性组件中添加FDE项的文档链接
    - 新增特性描述链接的CSS悬停效果
    - 移除未使用的示例特性，简化代码

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,41 +1,42 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'FDE',
     // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+      Finite Difference Eigenmode
       </>
     ),
+    link: '/docs/M_file/FDE/intro',
   },
-  {
-    title: 'Focus on What Matters',
-    // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  // {
+  //   title: 'Focus on What Matters',
+  //   // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+  //       ahead and move your docs into the <code>docs</code> directory.
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Powered by React',
+  //   // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Extend or customize your website layout by reusing React. Docusaurus can
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
 ];
 
-function Feature({ title, description}) {
+function Feature({ title, description, link}) {
   return (
     <div className={clsx('col col--4')}>
       {/* <div className="text--center">
@@ -43,7 +44,14 @@ function Feature({ title, description}) {
       </div> */}
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+        {link ? (
+          <Link to={link} className={styles.featureDescriptionLink}>
+            <p>{description}</p>
+          </Link>
+        ) : (
+          <p>{description}</p>
+        )}
+        
       </div>
     </div>
   );
